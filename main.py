007bf50fe2e5253c5e76efc4ff239f6e28c6b3a8
Anand Krishnan <anandkrishnan1835@gmail.com>
2023-03-14 19:57:08 -0700

    Pushed code to take picture when face detected

--- main.py
+++ main.py
@@ -1,44 +1,70 @@
+#I changed the system last minute so ignore all warnings about buggy code
+
+
+
 import cv2
 import numpy as np
 
 # Load the pre-trained Haar Cascade classifiers for frontal and profile faces
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
+#face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+#profile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_profileface.xml')
 
 # Open a video capture object for the webcam
 cap = cv2.VideoCapture(0)
+
 
 while True:
     # Read a frame from the webcam
     ret, frame = cap.read()
 
-    # Create a black mask with the same size as the frame
-    mask = np.zeros_like(frame)
+    
+    
 
-    # Convert the frame to grayscale
+    
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Detect frontal faces in the grayscale frame
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
 
-    # Copy detected frontal faces from the frame to the mask
-    for (x,y,w,h) in faces:
-        mask[y:y+h,x:x+w] = frame[y:y+h,x:x+w]
+    profileCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_profileface_default.xml")
+    profile = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+    )
 
-    # Detect profile faces in the grayscale frame
-    profiles = profile_cascade.detectMultiScale(gray, 1.3, 5)
+    print("[INFO] Found {0} Faces!".format(len(faces)))
+    print("[INFO] Found {0} Faces!".format(len(profile)))
 
-    # Copy detected profile faces from the frame to the mask
-    for (x,y,w,h) in profiles:
-        mask[y:y+h,x:x+w] = frame[y:y+h,x:x+w]
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
+    for (x, y, w, h) in profile:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        
+
+    status = cv2.imwrite('faces_detected.jpg', frame)
+    print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
+
+
+
+
+    
 
     # Display the resulting mask with face detections
-    cv2.imshow('Face Detection',mask)
+    cv2.imshow('Face Detection',frame)
 
-    # Exit if the 'q' key is pressed
+   
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release the video capture and destroy all windows
+
 cap.release()
 cv2.destroyAllWindows()
